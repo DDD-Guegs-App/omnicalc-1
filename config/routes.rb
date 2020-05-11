6264ed460c4f72d899a3e5488135fbce6c815247
guiguegs <gui.rfranzoni@gmail.com>
2020-05-11 22:32:51 +0000

    Added Square

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
+  # get("",{:controller => "application", :action => ""})
+  get("square/new",{:controller => "application", :action => "blank_square_form"})
+  get("add/new",{:controller => "application", :action => "blank_square_form"})
+  #get("square/new",{:controller => "application", :action => "blank_square_form"})
+  #get("square/new",{:controller => "application", :action => "blank_square_form"})
+
   # Write your routes above this line
   # =================================
 
